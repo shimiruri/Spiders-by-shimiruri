c1ccc3bd3a071d61dfa0e6415782f88618d86ef6
志村平秀 <41684763+shimiruri@users.noreply.github.com>
2018-09-16 22:37:33 +0800

    Update middlewares.py

--- steam/steam/middlewares.py
+++ steam/steam/middlewares.py
@@ -8,7 +8,6 @@
 from scrapy import signals
 
 
-
 class SteamSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
