0b8365cba4b4772ee90de6b049625438844b3b7d
志村平秀 <41684763+shimiruri@users.noreply.github.com>
2018-10-08 23:35:11 +0800

    Update pipelines.py

--- movies250/movies250/pipelines.py
+++ movies250/movies250/pipelines.py
@@ -4,44 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
 import json
-from movies250.items import *
-
-
-class MongoPipeline(object):
-
-	collection1 = "Basic_infos"
-	collection2 = "Reviews_detials"
-
-	def __init__(self, mongo_uri, mongo_db):
-		self.mongo_uri = mongo_uri
-		self.mongo_db = mongo_db
-
-	@classmethod
-	def from_crawler(cls, crawler):
-		"""通过from_crawler类方法获取settings的信息"""
-		return cls(
-			mongo_uri=crawler.settings.get('MONGO_URI'),
-			mongo_db=crawler.settings.get('MONGO_DB')
-			)
-
-	def open_spider(self, spider):
-		"""创建MongoDB的client并连接db"""
-		self.client = pymongo.MongoClient(self.mongo_uri)
-		self.db = self.client[self.mongo_db]
-
-	def process_item(self, item, spider):
-		"""官方推荐用insert_one()和insert_many,结果返回Result对象"""
-		if item == SurfaceItem():
-			self.db[self.collection1].insert(dict(item))
-			return item
-		elif item == CommentItem():
-			self.db[self.collection2].insert(dict(item))
-			return item
-
-	def close_spider(self, spider):
-		self.client.close()
 
 
 class MoviesPipeline(object):
